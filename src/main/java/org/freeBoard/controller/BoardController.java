0cc701b0992d4ec31716d4296fc19d68d3dc655b
sh <madst0614@gmail.com>
2022-11-16 18:51:50 +0900

    list, get, register.jsp(done)

--- src/main/java/org/freeBoard/controller/BoardController.java
+++ src/main/java/org/freeBoard/controller/BoardController.java
@@ -7,8 +7,10 @@ import org.freeBoard.service.BoardService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import lombok.AllArgsConstructor;
@@ -33,15 +35,28 @@ public class BoardController {
 		model.addAttribute("pageMaker", new PageDTO(cri, total));
 	}
 
+	@GetMapping("/register")
+	public void register() {
+
+	}
+
 	@PostMapping("/register")
 	public String register(BoardVO board, RedirectAttributes rttr) {
 
 		log.info("register: " + board);
 
 		service.register(board);
-		
+
 		rttr.addFlashAttribute("result", board.getBno());
 
 		return "redirect:/board/list";
 	}
+
+	@GetMapping({ "/get", "/modify" })
+	public void get(@RequestParam("bno") Long bno, @ModelAttribute("cri") Criteria cri, Model model) {
+
+		log.info("/get or /modify");
+		model.addAttribute("board", service.get(bno));
+	}
+
 }
