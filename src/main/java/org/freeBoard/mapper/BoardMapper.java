c500cc4d147168f4af1af19441af17b37ecea5f0
sh <madst0614@gmail.com>
2022-11-19 04:04:40 +0900

    Reply : VO, PageDTO(done)Mapper(done), Service(done), Serviceimp(done)

--- src/main/java/org/freeBoard/mapper/BoardMapper.java
+++ src/main/java/org/freeBoard/mapper/BoardMapper.java
@@ -2,6 +2,7 @@ package org.freeBoard.mapper;
 
 import java.util.List;
 
+import org.apache.ibatis.annotations.Param;
 import org.freeBoard.domain.BoardVO;
 import org.freeBoard.domain.Criteria;
 
@@ -18,5 +19,7 @@ public interface BoardMapper {
 	public int getTotalCount(Criteria cri);
 	
 	public List<BoardVO> getListWithPaging(Criteria cri);
+	
+	public void updateReplyCnt(@Param("bno") Long bno, @Param("amount") int amount);
 	
 }
---
+++ src/main/java/org/freeBoard/service/ReplyServiceImpl.java
@@ -0,0 +1,80 @@
+package org.freeBoard.service;
+
+import java.util.List;
+
+import org.freeBoard.domain.Criteria;
+import org.freeBoard.domain.ReplyPageDTO;
+import org.freeBoard.domain.ReplyVO;
+import org.freeBoard.mapper.BoardMapper;
+import org.freeBoard.mapper.ReplyMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import lombok.Setter;
+import lombok.extern.log4j.Log4j2;
+
+@Service
+@Log4j2
+public class ReplyServiceImpl implements ReplyService {
+	
+	@Setter(onMethod_=@Autowired)
+	private ReplyMapper mapper;
+	
+	@Setter(onMethod_=@Autowired)
+	private BoardMapper boardMapper;
+	
+	@Transactional
+	@Override
+	public int register(ReplyVO vo) {
+		log.info("register" + vo);
+		
+		boardMapper.updateReplyCnt(vo.getBno(), 1);
+		
+		return mapper.insert(vo);
+	}
+
+	@Override
+	public ReplyVO get(Long rno) {
+		// TODO Auto-generated method stub
+		log.info("get"+rno);
+		
+		return mapper.read(rno);
+	}
+
+	@Override
+	public int modify(ReplyVO vo) {
+		log.info("modify"+vo);
+		
+		return mapper.update(vo);
+	}
+	
+	@Transactional
+	@Override
+	public int remove(Long rno) {
+		// TODO Auto-generated method stub
+		
+		ReplyVO vo = mapper.read(rno);
+		boardMapper.updateReplyCnt(vo.getBno(), -1);
+		
+		return mapper.delete(rno);
+	}
+
+	@Override
+	public List<ReplyVO> getList(Criteria cri, Long bno) {
+		// TODO Auto-generated method stub
+		
+		log.info("Reply List" + bno);
+		
+		return mapper.getListWithPaging(cri, bno);
+	}
+
+	@Override
+	public ReplyPageDTO getListPage(Criteria cri, Long bno) {
+		// TODO Auto-generated method stub
+		return new ReplyPageDTO(mapper.getCount(bno), mapper.getListWithPaging(cri, bno));
+	}
+	
+	
+	
+}
